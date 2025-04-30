ed695f8a4bf221a185b9f40bf9e6a74ad8e2f117
franztac <fratacc91@gmail.com>
2025-04-30 12:54:26 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='water potability preds using ML... code and data versioning',
+    author='franztac',
+    license='',
+)
